4302d65284062698a7fbfde9069addc577bd89c2
Yue Zhao <yuezhao512@gmail.com>
2024-01-20 13:49:50 -0600

    CHANGE

--- R/remfpcaClass.R
+++ R/remfpcaClass.R
@@ -53,103 +53,7 @@
 #' @importFrom Matrix bdiag
 #' @importFrom expm sqrtm
 #' @export
-#' remfpca <- R6::R6Class(
-#'   "remfpca",
-#'   public = list(
-#'     #' @param mvmfd_obj An `mvmfd` object representing the multivariate functional data.
-#'     #' @param ncomp The number of functional principal components to retain.
-#'     #' @param alpha A list or vector specifying the regularization parameter(s) for each variable.
-#'     #'              If NULL, the regularization parameter is estimated internally.
-#'     #' @param centerfns Logical indicating whether to center the functional data before analysis.
-#'     #' @param alpha_orth Logical indicating whether to perform orthogonalization of the regularization parameters.
-#'     #' @param penalty_type The type of penalty to be applied on the coefficients. The types "coefpen" and "basispen" is supported. Default is "coefpen".
-#'     initialize = function(mvmfd_obj, ncomp, alpha = NULL, centerfns = TRUE, alpha_orth = TRUE, penalty_type = "coefpen") {
-#'       if (is.numeric(alpha)) {
-#'         if (is.mfd(mvmfd_obj)) {
-#'           alpha <- list(alpha)
-#'         } else {
-#'           alpha <- as.list(alpha)
-#'         }
-#'       }
-#'       if (is.mfd(mvmfd_obj)) mvmfd_obj <- Mvmfd(mvmfd_obj)
-#'       result <- eigen_approach(mvmfd_obj = mvmfd_obj, n = ncomp, alpha = alpha, centerfns = centerfns, penalty_type = penalty_type)
-#'       coef <- result[[1]]
-#'       pcmfd <- list()
-#'       for (i in 1:mvmfd_obj$nvar) {
-#'         if (mvmfd_obj$basis$dimSupp[i] == 1) {
-#'           pcmfd[[i]] <- Mfd(X = coef[[i]], mdbs = mvmfd_obj$basis$basis[[i]], method = "coefs")
-#'         } else {
-#'           coef_new <- array(coef[[i]], dim = c(mvmfd_obj$basis$basis[[i]]$nbasis, ncol(coef[[i]])))
-#'           pcmfd[[i]] <- Mfd(X = coef_new, mdbs = mvmfd_obj$basis$basis[[i]], method = "coefs")
-#'         }
-#'       }
-#'       out <- Mvmfd(pcmfd)
-#'       if (mvmfd_obj$nvar == 1) {
-#'         private$.pc_mfd <- pcmfd[[1]]
-#'       } else {
-#'         private$.pc_mfd <- Mvmfd(pcmfd)
-#'       }
-#'       private$.lsv <- result[[2]]
-#'       private$.values <- result[[3]]
-#'       private$.alpha <- result[[4]]
-#'       private$.GCVs <- result[[5]]
-#'       private$.mean_mfd <- mean(mvmfd_obj)
-#'     }
-#'   ),
-#'   active = list(
-#'     pc_mfd = function(value) {
-#'       if (missing(value)) {
-#'         private$.pc_mfd
-#'       } else {
-#'         stop("`$pc_mfd` is read only", call. = FALSE)
-#'       }
-#'     },
-#'     lsv = function(value) {
-#'       if (missing(value)) {
-#'         private$.lsv
-#'       } else {
-#'         stop("`$lsv` is read only", call. = FALSE)
-#'       }
-#'     },
-#'     values = function(value) {
-#'       if (missing(value)) {
-#'         private$.values
-#'       } else {
-#'         stop("`$sigma` is read only", call. = FALSE)
-#'       }
-#'     },
-#'     alpha = function(value) {
-#'       if (missing(value)) {
-#'         private$.alpha
-#'       } else {
-#'         stop("`$alpha` is read only", call. = FALSE)
-#'       }
-#'     },
-#'     GCVs = function(value) {
-#'       if (missing(value)) {
-#'         private$.GCVs
-#'       } else {
-#'         stop("`$GCVs` is read only", call. = FALSE)
-#'       }
-#'     },
-#'     mean_mfd = function(value) {
-#'       if (missing(value)) {
-#'         private$.mean_mfd
-#'       } else {
-#'         stop("`$mean_mfd` is read only", call. = FALSE)
-#'       }
-#'     }
-#'   ),
-#'   private = list(
-#'     .pc_mfd = NULL,
-#'     .lsv = NULL,
-#'     .values = NULL,
-#'     .alpha = NULL,
-#'     .GCVs = NULL,
-#'     .mean_mfd = NULL
-#'   )
-#' )
-s_power_class <- R6::R6Class("s_power_class",
+remfpca <- R6::R6Class("remfpca",
                              public = list(
                                initialize = function(mvmfd_obj, method = "power", ncomp, smooth_tuning = NULL,sparse_tuning = NULL, centerfns = TRUE, alpha_orth = TRUE,smooth_tuning_type = "coefpen",sparse_tuning_type = "soft",K_fold,cv_type = "joint") {
                                  if (is.numeric(smooth_tuning)) smooth_tuning <- as.list(smooth_tuning)
@@ -237,7 +141,7 @@ s_power_class <- R6::R6Class("s_power_class",
 #' @rdname remfpca
 #' @seealso \code{\link{mvmfd}}
 
-#' @title A Class for 'ReMFPCA' objects
+#' @title A Class for 'remfpca' objects
 #'
 #' @description
 #' The `remfpca` class represents regularized functional principal components ('ReMFPCs') components.
@@ -250,7 +154,7 @@ s_power_class <- R6::R6Class("s_power_class",
 #' @param alpha_orth Logical indicating whether to perform orthogonalization of the regularization parameters.
 #' @param penalty_type The type of penalty to be applied on the coefficients. The types "coefpen" and "basispen" is supported. Default is "coefpen".
 #' @export
-Remfpca_fsvd <- function(mvmfd_obj, method = "power", ncomp, smooth_tuning = NULL,sparse_tuning = NULL, centerfns = TRUE, alpha_orth = TRUE,smooth_tuning_type = "coefpen",sparse_tuning_type = "soft",K_fold,cv_type = "joint") {
-  s_power_class$new(mvmfd_obj, method = "power", ncomp, smooth_tuning = NULL,sparse_tuning = NULL, centerfns = TRUE, alpha_orth = TRUE,smooth_tuning_type = "coefpen",sparse_tuning_type = "soft",K_fold,cv_type = "joint")
+Remfpca <- function(mvmfd_obj, method = "power", ncomp, smooth_tuning = NULL,sparse_tuning = NULL, centerfns = TRUE, alpha_orth = TRUE,smooth_tuning_type = "coefpen",sparse_tuning_type = "soft",K_fold,cv_type = "joint") {
+  remfpca$new(mvmfd_obj, method = "power", ncomp, smooth_tuning = NULL,sparse_tuning = NULL, centerfns = TRUE, alpha_orth = TRUE,smooth_tuning_type = "coefpen",sparse_tuning_type = "soft",K_fold,cv_type = "joint")
 }
 #' @rdname remfpca
